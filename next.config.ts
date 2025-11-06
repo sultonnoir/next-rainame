08076067d25cd2b041b163435dccd011dfd985f7
sulton <sultonoir@gmail.com>
2025-11-06 16:46:36 +0700

    fix : error next 16 + prisma

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  outputFileTracingIncludes: {
+    "/api/**/*": ["./node_modules/.prisma/client/**/*"],
+    "/**/*": ["./node_modules/.prisma/client/**/*"],
+  },
   cacheComponents: true,
   reactCompiler: true,
 };
